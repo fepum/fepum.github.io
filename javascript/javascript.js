fe6c2d1edbadf9e0b725817ef0391fb3a16d09c7
fi'oj'9ioj <npijkn0ip>
2020-06-03 18:39:40 -0500

    primer commit

---
+++ javascript/javascript.js
@@ -0,0 +1,4 @@
+const myHeading = document.querySelector('a');
+myHeading.textContent = '!hola , artezana';document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
