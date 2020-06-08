61ea8e86387c0aa62531b6a59a728b09b4f37b7f
fepum <fervius@hotmail.com>
2020-06-08 16:40:34 -0500

    sin javascript

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,1 @@
-const myHeading = document.querySelector('a');
-myHeading.textContent = '!hola , artezana';document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+
